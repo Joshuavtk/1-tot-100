1ef18115c22cabada5393c89b5f3cc1c2660b6eb
Joshua <joshuavtk@hotmail.com>
2017-10-06 12:16:27 +0200

    finished

---
+++ java.js
@@ -0,0 +1,15 @@
+for (let i = 1; i <= 100; i++) {
+    let output = 0;
+    if (i % 3 == 0){
+        output = 1;
+    }
+    if (i % 5 == 0){
+        if (output == 1) {
+            output = 3;
+        } else {
+            output = 2;
+        }
+    }
+    let messages = [i,"Media","College","MediaCollege"];
+    console.log(messages[output])
+}
